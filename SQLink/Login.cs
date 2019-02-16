2bf93bff313223c4cabb133551b805e279e09cc3
Maurycy Graclik <m.graclik@gmail.com>
2019-02-16 10:11:21 +0100

    Jest git

--- SQLink/Login.cs
+++ SQLink/Login.cs
@@ -22,39 +22,40 @@ namespace SQLink
         //SqlConnection WinAuth = new SqlConnection(@"Data Source=SRV-SQL5;Initial Catalog=SQLinkDB;Integrated Security=True");
         private void button2_Click(object sender, EventArgs e)  //łączenie z bazą danych przy użyciu wprowadzonych danych (sa we bazie w tabeli)
         {
-            try
-            {
-                DbConnection.Initlizlie("SRV-SQL5", "SQLinkDB", IDtextBox.Text, PasstextBox.Text);
-
-
-                using (SqlConnection conn = new SqlConnection(DbConnection.ConnectionString)) //testuje czy działa połączenie
-                {
-
-                    conn.Open();
-
-
-                }
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("SQL not responding");
-                return;
-            };
-
-            SqlConnection LogMEin = new SqlConnection(DbConnection.ConnectionString);
-            string query = "Select * from SQLogin where username = '" + IDtextBox.Text.Trim() + "' and password = '" + PasstextBox.Text.Trim() + "'";
-            SqlDataAdapter adapter1 = new SqlDataAdapter(query, LogMEin);
-            DataTable Tab1 = new DataTable();
-            adapter1.Fill(Tab1);
-
-      //        greatJob B1 = new greatJob();
-              this.Hide();
-      //        B1.Show();
             
-            Main M1 = new Main();
-            // uruchamia okno Main po prawidłowym logowaniu
-            M1.Show();
-        
+           DbConnection.Initlizlie(srvnamebox.Text, dbnamebox.Text, IDtextBox.Text, PasstextBox.Text);
+           using (SqlConnection conn = new SqlConnection(DbConnection.ConnectionString)) //testuje czy działa połączenie
+
+                try
+                {
+                    conn.Open();
+                }
+                    catch (Exception ex)
+                    {
+                    MessageBox.Show(ex.Message);
+                    conn.Close();
+                    DbConnection.isInitilized = false;
+                    return;
+                    };
+
+
+            //to ponizej potencjalnie zbedne
+            //SqlConnection LogMEin = new SqlConnection(DbConnection.ConnectionString);
+            //string query = "Select * from SQLogin where username = '" + IDtextBox.Text.Trim() + "' and password = '" + PasstextBox.Text.Trim() + "'";
+            //SqlDataAdapter adapter1 = new SqlDataAdapter();
+            //SqlDataAdapter adapter1 = new SqlDataAdapter(query, LogMEin);
+            //DataTable Tab1 = new DataTable();
+            //adapter1.Fill(Tab1);
+
+
+            //greatJob B1 = new greatJob();
+            //  B1.Show();
+
+            MessageBox.Show("You are now connected to " + srvnamebox.Text,
+                "Server Info");
+            this.Hide();
+            Main C1 = new Main();
+            C1.Show();
         }
 
         private void IDtextBox_TextChanged(object sender, EventArgs e)  //okienko na ID
@@ -84,11 +85,43 @@ namespace SQLink
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)  //checkbox windows auth 
         {
-
+            if (checkBoxAD.Checked)
+            {
+                //Połączenie ma być domenowe
+            }
+            else 
+            {
+                //Połączenie po SQL auth
+            }
         }
 
         private void Login_Load(object sender, EventArgs e)  //okno login ogólne 
         {
+
+        }
+
+        private void srvnamebox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ADConnButton_Click(object sender, EventArgs e)
+        {
+            ADConnection.ADInitlizlie(srvnamebox.Text, dbnamebox.Text, IDtextBox.Text, PasstextBox.Text);
+            using (SqlConnection connAD = new SqlConnection(ADConnection.ADConnectionString)) //testuje czy działa połączenie
+
+                try
+                {
+                    connAD.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    connAD.Close();
+                    ADConnection.isADInitilized = false;
+                    return;
+                };
+
 
         }
     }
